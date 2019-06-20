145debc7331034c40fa5a5daa53f9239c6681885
De Coning <Travis.DeConing@affinitywater.co.uk>
2019-06-20 09:02:10 +0100

    Initial upload

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_account_id" {}
+variable "aws_profile" {}
+variable "aws_region" {}
+
+variable "bucket_prefix" {
+  default = "awl-config"
+}
+
+variable "bucket_key_prefix" {
+  default = "awl-config"
+}
+
+variable "sns_topic_arn" {
+  default = "arn:aws:sns:eu-west-2:889199313043:security-alerts-topic"
+}
+
+variable "tags" {
+  default = {
+    "owner"   = "Infrastructure"
+    "project" = "AWL-Config"
+    "client"  = "Internal"
+  }
+}
