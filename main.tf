695911333380157713187785dcfff3327203a291
De Coning <Travis.DeConing@affinitywater.co.uk>
2019-06-20 12:07:16 +0100

    remove provider

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
+/*
 provider "aws" {
   region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
 }
-
+*/
 
 # -----------------------------------------------------------
 # set up the  Config Recorder
