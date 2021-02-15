f5d1b6055b4791571ae1bd6e5221b851596da511
Glenn Haddrell <glenn.haddrell@affinitywater.co.uk>
2021-02-15 16:10:40 +0000

    Add config aggregator

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
 data "aws_caller_identity" "current" {
 }
 
+data "aws_region" "current" {}
+
 variable "bucket_prefix" {
   default = "awl-config"
 }
@@ -19,6 +21,20 @@ variable "config_name" {
 
 variable "sns_topic_arn" {
   default = ""
+}
+
+variable "aggregator_account_id" {
+  default = ""
+}
+
+variable "account_aggregation_source_account_ids"{
+  type = list
+  default = []
+}
+
+variable "account_aggregation_source_regions"{
+  type = list
+  default = []
 }
 
 variable "tags" {
